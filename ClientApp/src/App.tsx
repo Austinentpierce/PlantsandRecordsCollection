c57d76e5f011e3eadedcf5122c84f8bb85fd362e
Austin Pierce <Austinentpierce@gmail.com>
2021-09-21 14:41:58 -0400

    adding html css for homepage

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 
-
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <main>
+      <header>
+        <div className=" leaf fas fa-3x fa-leaf"></div>
+        <div className="mainhead">BP and R</div>
+        <div className=" vinyl fas fa-3x fa-record-vinyl"></div>
+      </header>
+      <h2>plants.</h2>
+      <img
+        src="https://d39l2hkdp2esp1.cloudfront.net/img/photo/174043/174043_00_2x.jpg?20190722043957"
+        width="275"
+        height="275"
+      ></img>
+      <h3>records.</h3>
+      <img
+        src="https://images.theconversation.com/files/78216/original/image-20150416-5628-6s1t8.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip"
+        width="275"
+        height="275"
+      ></img>
+      <footer>Created with </footer>
+    </main>
+  )
 }
