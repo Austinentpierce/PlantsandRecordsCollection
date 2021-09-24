6de92e150959018f6344312b3a92e94a9e2e17ae
Austin Pierce <Austinentpierce@gmail.com>
2021-09-24 11:11:32 -0400

    displayed seed data

--- ClientApp/src/Pages/ViewPlants.tsx
+++ ClientApp/src/Pages/ViewPlants.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { PlantType } from '../types'
 
 export function ViewPlants() {
+  const { data: Plants = [] } = useQuery<PlantType[]>(
+    'plants',
+    async function () {
+      const response = await fetch('/api/Plants')
+      return response.json()
+    }
+  )
+  console.log({ Plants })
   return (
     <main className="PlantsPage">
       <input className="plantsearch" placeholder="Search Plants" />
+
+      <ul className="DiffPlants">
+        {Plants.map(function (Plants) {
+          return (
+            <li key={Plants.id}>
+              <h2>{Plants.name}</h2>
+              <p>{Plants.type}</p>
+              <p>{Plants.location}</p>
+              <p>{Plants.watering}</p>
+              <p>{Plants.pot}</p>
+              <p>{Plants.description}</p>
+            </li>
+          )
+        })}
+      </ul>
     </main>
   )
 }
---
+++ ClientApp/src/types.ts
@@ -0,0 +1,9 @@
+export type PlantType = {
+  id: number
+  name: string
+  type: string
+  location: string
+  watering: string
+  pot: number
+  description: string
+}
