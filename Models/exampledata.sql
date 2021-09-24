3e391ccb2842e08c4e8ff79f9bb47da80010a1e1
Austin Pierce <Austinentpierce@gmail.com>
2021-09-23 20:36:22 -0400

    created sample data

---
+++ Models/exampledata.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE "Plants" RESTART IDENTITY;
+
+INSERT INTO "Plants" ("Name", "Type", "Location", "Watering", "Pot", "Description") VALUES ('Fig', 'Normal', 'Outdoor', 'Every other day', 5, 'Medium size stems and round leaves')
+INSERT INTO "Plants" ("Name", "Type", "Location", "Watering", "Pot", "Description") VALUES ('Mango', 'Fruit', 'Outdoor', 'Every other day', 3, 'Tall plant and thin leaves')
+INSERT INTO "Plants" ("Name", "Type", "Location", "Watering", "Pot", "Description") VALUES ('Monstera', 'Normal', 'Outdoor', 'Every other day', 6, 'Medium size plant had slits in leave typically 2-8 slits in each leaf')
+INSERT INTO "Plants" ("Name", "Type", "Location", "Watering", "Pot", "Description") VALUES ('Red pepper', 'Vegetable', 'Outdoor', 'Every other day', 1, 'small plant 1 foot tall small leaves white flowers before it blooms')
