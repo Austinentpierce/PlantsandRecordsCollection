50f94adfd45d5423094805053c2fb2916524c7ec
Austin Pierce <Austinentpierce@gmail.com>
2021-09-23 20:12:29 -0400

    created models and migration and table for plants

---
+++ Migrations/20210923234252_CreatePlant.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PlantsandRecordsCollection.Migrations
+{
+    public partial class CreatePlant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<string>(type: "text", nullable: true),
+                    Location = table.Column<string>(type: "text", nullable: true),
+                    Watering = table.Column<string>(type: "text", nullable: true),
+                    Pot = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plants", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plants");
+        }
+    }
+}
---
+++ Models/Plants.cs
@@ -0,0 +1,14 @@
+namespace PlantsandRecordsCollection.Models
+{
+    public class Plants
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Watering { get; set; }
+        public int Pot { get; set; }
+        public string Description { get; set; }
+
+    }
+}
