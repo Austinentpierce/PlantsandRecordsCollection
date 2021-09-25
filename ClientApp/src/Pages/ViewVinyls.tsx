7f483bb220a6f15213ee4ffe9c4074d9d9cf7cc4
Austin Pierce <Austinentpierce@gmail.com>
2021-09-25 14:14:45 -0400

    changed background on vinyls page

--- ClientApp/src/Pages/ViewVinyls.tsx
+++ ClientApp/src/Pages/ViewVinyls.tsx
@@ -19,7 +19,7 @@ export function ViewVinyls() {
         {Vinyls.map(function (Vinyls) {
           return (
             <li key={Vinyls.id}>
-              <h2 className="VinylName">The name of the album is</h2>
+              <h2 className="VinylName">{Vinyls.album}</h2>
               <p>{Vinyls.artist}</p>
               <p>{Vinyls.releaseYear}</p>
               <p>{Vinyls.genre}</p>
