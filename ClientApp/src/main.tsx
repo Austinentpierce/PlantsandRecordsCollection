56ddd8a602fb4ea2840493824de1be24e114a5b5
Austin Pierce <Austinentpierce@gmail.com>
2021-09-29 13:48:49 -0400

    Easier to access certain scss for each category

--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -4,6 +4,10 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { App } from './App'
 import './index.scss'
+import './Crystals.scss'
+import './Vinyls.scss'
+import './Plants.scss'
+import './SignUpandIn.scss'
 
 const queryClient = new QueryClient()
 
